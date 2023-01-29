64d3d764c852dae2145abc1a8293e7a20edb0a6b
Sam Cassidy <sam@cassidys.email>
2023-01-29 06:23:31 +0000

    extracts urls from ddg results and matches with configured sources. launches the browser with the result

--- src/docter.py
+++ src/docter.py
@@ -1,30 +1,64 @@
 import yaml
-import requests
+import subprocess
 import re
 from urllib.request import Request, urlopen
-from bs4 import BeautifulSoup
 
 
 def main():
-    pass
+    results = ddg_search("python calendar")
+    urls = [url for url in result_urls(results)]
+    config = load_config("test/test.yaml")
+    keyword = "python"
+    for url in urls:
+        if match := match_url_with_source_config(url, keyword, config):
+            break
+    launch_page("w3m", url)
 
-def load_config(path):
+
+def match_url_with_source_config(url: str, keyword: str, config: dict) -> str:
+    keyword_sources = config["keywords"][keyword]["sources"]
+    for source in keyword_sources:
+        try:
+            pattern = config['sources'][source]['url']
+        except KeyError:
+            print(f"Warning: source {source} is not configured")
+        if pattern in url:
+            return source
+
+def launch_page(browser: str, url: str, gui: bool=False) -> None:
+    subprocess.run([browser, url])
+
+
+
+def load_config(path: str) -> dict:
     with open(path, 'r', encoding='utf8') as f:
         config = yaml.load(f, Loader=yaml.Loader)
     return config
 
 
-def result_urls(response):
-    for line in response.splitlines():
+def result_urls(results_html: str) -> str:
+    for line in results_html.splitlines():
         exp = '(?:result__url.+)(http.+)"'
         match = re.search(exp, line) 
         if match:
             yield match.group(1)
 
 
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-req = Request("https://duckduckgo.com/html/?q=hello", headers=headers)
-response = urlopen(req).read().decode()
-urls = [url for url in result_urls(response)]
-print(urls)
+def ddg_search(search_string: str) -> str:
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    search = '+'.join(search_string.split())
+    req = Request(f"https://duckduckgo.com/html/?q={search}", headers=headers)
+    return urlopen(req).read().decode()
 
+def load_config(path: str) -> dict:
+    with open(path, "r", encoding="utf8") as f:
+        try:
+            config = yaml.safe_load(f)
+        except yaml.YAMLError as exc:
+            print(exc)
+    return config
+
+
+
+if __name__ == "__main__":
+    main()
