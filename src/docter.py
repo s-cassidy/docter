9c5b6e8164358f2554feb5c0ac47fffb6e53e5e8
Sam Cassidy <sam@cassidys.email>
2023-01-31 22:55:06 +0000

    refactored configuration into a class. cleaning still needed

--- src/docter.py
+++ src/docter.py
@@ -1,44 +1,99 @@
+#!/usr/bin/env python3
 import yaml
 import subprocess
 import argparse
 import re
 from urllib.request import Request, urlopen
+from pathlib import Path
 
 
 def main():
     args = get_arguments()
-    config = load_config("test/test.yaml")
-    check_keyword(args, config)
-    search_str = build_search_string(args, config)
+    config_path = f"{str(Path.home())}/.config/docter.yaml"
+    instance_config = load_config(config_path, args)
+    search_str = instance_config.get_search_string()
     search_result_page = ddg_search(search_str)
     results = [result for result in get_result_urls(search_result_page)]
-    result_url, browser = select_result_and_browser(results, args, config)
+    result_url, browser = select_result_and_browser(results, instance_config)
     if result_url:
         launch_page(browser, result_url)
 
 
-def check_keyword(args: argparse.Namespace, config: dict) -> None:
-    keyword = args.keyword
-    if keyword not in config['keywords']:
-        print(f"{keyword} is not a keyword; performing a standard search with default settings")
-        config['keywords'][keyword] = {'terms': [keyword],
-                                       'sources': ['any']}
-        config["sources"]["any"] = {'urlpattern': ''}
+def get_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("keyword",
+                        help="configurable keyword to select sources")
+    parser.add_argument("terms", 
+                        help="terms to search for in the selected sources",
+                        nargs=argparse.REMAINDER)
+    return parser.parse_args()
 
 
-def select_result_and_browser(results: list,
-                              args: argparse.Namespace,
-                              config: dict) -> tuple:
+class InstanceConfig:
+    def __init__(self, global_config, args) -> None:
+        self.keyword = args.keyword
+        self.handle_no_keyword(global_config)
+        self.defaultbrowser = global_config['defaultbrowser']
+        self.defaultgui = global_config['defaultgui']
+        self.gui_browser = False
+        self.terms = args.terms
+        self.added_terms = global_config['keywords'][self.keyword]['terms']
+        self.sources = self.make_sources_dict(global_config)
+        print(self.sources)
+        self.additional_terms = global_config['keywords'][self.keyword]['terms']
+
+    def make_sources_dict(self, global_config: dict) -> dict:
+        keyword_sources = global_config['keywords'][self.keyword]['sources']
+        all_sources = global_config['sources']
+        sources_dict = dict()
+        for source in keyword_sources:
+            if source not in all_sources:
+                print(f"{word} source {source} not properly configured")
+            else:
+                sources_dict[source] = all_sources[source]
+        return sources_dict
+
+    def handle_no_keyword(self, config: dict) -> None: 
+        keyword = self.keyword
+        if keyword not in config['keywords']:
+            print(f"{keyword} is not a keyword;"
+                  " performing a standard search with default settings")
+            config['keywords'][''] = {'terms': [keyword],
+                                      'sources': ['any']}
+            config['sources']['any'] = {'urlpattern': ''}
+            self.keyword = ''
+
+    def get_search_string(self) -> str:
+        search_str = ' '.join(self.added_terms+self.terms)
+        print(f"Searching for {search_str}")
+        return search_str
+
+    def match_url_with_source(self, url: str) -> str:
+        for source_name, source_info in self.sources.items():
+            try:
+                pattern = source_info['urlpattern']
+            except KeyError:
+                print(f"Warning: source {source_name} is not configured")
+            if pattern in url:
+                return source_name
+
+
+def select_result_and_browser(results: list, config) -> tuple:
     for result in results:
-        if source := match_url_with_source(result, args.keyword, config):
+        if source := config.match_url_with_source(result):
             browser = select_browser(source, config)
             if accept_page_launch(result, browser):
                 return result, browser
     else:
-        print(f"No other likely results found from {args.keyword}"
-              f" sources for {args.terms}."
+        print(f"No other likely results found from {config.keyword}"
+              f" sources for {config.terms}."
               )
         return None, browser
+
+
+def select_browser(source: str, config) -> str:
+    default = config.defaultbrowser
+    return config.sources[source].get('browser', default)
 
 
 def accept_page_launch(url: str, browser: str) -> tuple:
@@ -50,41 +105,28 @@ def accept_page_launch(url: str, browser: str) -> tuple:
             return True
 
 
-def select_browser(source: str, config: dict) -> str:
-    default = config['defaultbrowser']
-    return config['sources'][source].get('browser', default)
-
-
-def build_search_string(cli_args: argparse.Namespace, config: dict) -> str:
-    keyword = cli_args.keyword
-    terms = cli_args.terms
-    additional_terms = config['keywords'][keyword]['terms']
-    search_str = '+'.join(additional_terms+terms.split())
-    return search_str
-
-
-def get_arguments() -> argparse.Namespace:
-    parser = argparse.ArgumentParser()
-    parser.add_argument("keyword",
-                        help="configurable keyword to select sources")
-    parser.add_argument("terms",
-                        help="terms to search for in the selected sources")
-    return parser.parse_args()
-
-
-def match_url_with_source(url: str, keyword: str, config: dict) -> str:
-    keyword_sources = config["keywords"][keyword]["sources"]
-    for source in keyword_sources:
+def load_config(path: str, keyword: str) -> dict:
+    with open(path, "r", encoding="utf8") as f:
         try:
-            pattern = config['sources'][source]['urlpattern']
-        except KeyError:
-            print(f"Warning: source {source} is not configured")
-        if pattern in url:
-            return source
+            global_config = yaml.safe_load(f)
+        except yaml.YAMLError as exc:
+            print(exc)
+    instance_config = InstanceConfig(global_config, keyword)
+    return instance_config
 
 
-def launch_page(browser: str, url: str, gui: bool = False) -> None:
-    subprocess.run([browser, url])
+
+def ddg_search(url_string: str) -> str:
+    headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X"
+                             " 10_10_1) AppleWebKit/537.36 (KHTML, "
+                             "like Gecko) Chrome/39.0.2171.95 Safari/537.36"
+               }
+    url_string = url_string.replace(' ', r'+')
+    req = Request(
+            f"https://duckduckgo.com/html/?q={url_string}",
+            headers=headers
+            )
+    return urlopen(req).read().decode()
 
 
 def get_result_urls(results_html: str) -> str:
@@ -94,36 +136,9 @@ def get_result_urls(results_html: str) -> str:
         if match:
             yield match.group(1)
 
+def launch_page(browser: str, url: str, gui: bool = False) -> None:
+    subprocess.run([browser, url])
 
-def ddg_search(url_string: str) -> str:
-    headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X"
-                             " 10_10_1) AppleWebKit/537.36 (KHTML, "
-                             "like Gecko) Chrome/39.0.2171.95 Safari/537.36"
-               }
-    print(f"Searching for {url_string}")
-    req = Request(
-            f"https://duckduckgo.com/html/?q={url_string}",
-            headers=headers
-            )
-    return urlopen(req).read().decode()
-
-
-def load_config(path: str) -> dict:
-    with open(path, "r", encoding="utf8") as f:
-        try:
-            config = yaml.safe_load(f)
-        except yaml.YAMLError as exc:
-            print(exc)
-    tidy_config(config)
-    return config
-
-
-def tidy_config(config: dict) -> None:
-    for word, word_opts in config['keywords'].items():
-        word_opts['terms'] = word_opts.get('terms', [word])
-        for source in word_opts['sources']:
-            if source not in config['sources']:
-                print(f"{word} source {source} not properly configured")
 
 if __name__ == "__main__":
     main()
